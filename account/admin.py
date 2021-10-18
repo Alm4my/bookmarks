551a076068d1ddccef7a92dfd1622d46385d65cc
Alm4my <thenixadm@gmail.com>
2021-10-18 23:54:42 +0000

    Register profile model in admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from account.models import Profile
+
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'photo']
+
