085bc5e89d1d8024831fb58c7f0aaae962fafb69
Alm4my <thenixadm@gmail.com>
2021-10-18 21:52:02 +0000

    Add Registration Form

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,25 @@
-from django.forms import Form, CharField, PasswordInput
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.forms import Form, CharField, PasswordInput, ModelForm
 
 
 class LoginForm(Form):
     username = CharField()
     password = CharField(widget=PasswordInput)
+
+
+class UserRegistrationForm(ModelForm):
+    password = CharField(label='Password',
+                         widget=PasswordInput)
+    password2 = CharField(label='Repeat Password',
+                          widget=PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'firstname', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise ValidationError('Passwords don\'t match.')
+        return cd['password2']
