4584ae5b4e007b2f3e71fbacc0226ffc3f1fe495
Alm4my <thenixadm@gmail.com>
2021-10-18 23:47:47 +0000

    Add profile model and url for media serving

--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import Model, CASCADE, DateField, ImageField
 
 # Create your models here.
+from bookmarks import settings
+
+
+class Profile(Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=CASCADE)
+
+    date_of_birth = DateField(blank=True, null=True)
+    photo = ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
