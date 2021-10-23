7c54c2b40a4bafcccc95ae737ec568a4051145be
Alm4my <thenixadm@gmail.com>
2021-10-23 15:45:17 +0000

    Override save method to allow form to download the image as it is being posted

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,8 @@
+from urllib import request
+
+from django.core.files.base import ContentFile
 from django.forms import ModelForm, HiddenInput
+from django.utils.text import slugify
 
 from account import forms
 from images.models import Image
@@ -21,3 +25,18 @@ class ImageCreateForm(ModelForm):
                                         'image extension.')
         return url
 
+    def save(self, force_insert=False, force_update=False ,commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
+                         save=False)
+        if commit:
+            image.save()
+        return image
