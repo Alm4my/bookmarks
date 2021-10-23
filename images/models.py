402138b4e42e5c664104f26073caf6072c6789f4
Alm4my <thenixadm@gmail.com>
2021-10-23 15:20:39 +0000

    Add model for images

--- images/models.py
+++ images/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.db.models import Model, ForeignKey, CASCADE, CharField, SlugField, URLField, ImageField, TextField, \
+    DateField, ManyToManyField
+from django.utils.text import slugify
 
-# Create your models here.
+from bookmarks import settings
+
+
+class Image(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL,
+                      related_name='images_created',
+                      on_delete=CASCADE)
+    title = CharField(max_length=200)
+    slug = SlugField(max_length=200, blank=True)
+    url = URLField()
+    image = ImageField(upload_to='images/%Y/%m/%d')
+    description = TextField(blank=True)
+    created = DateField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    users_like = ManyToManyField(settings.AUTH_USER_MODEL,
+                                 related_name='images_liked',
+                                 blank=True)
