2bce4a7e45314b993587c4920250802da99677fb
Alm4my <thenixadm@gmail.com>
2021-10-23 23:09:57 +0000

    Fix bookmarklet.js bug
    Bookmarklet now show us when we click on link

--- templates/images/bookmarklet_launcher.js
+++ templates/images/bookmarklet_launcher.js
@@ -1,9 +1,8 @@
-(function () {
+(function(){
     if (window.myBookmarklet !== undefined){
         myBookmarklet();
     }
-    else{
-        document.body.appendChild(document.createElement('script'))
-            .src(`https://127.0.0.1/static/js/bookmarklet.js?r=${Math.floor(Math.random()*999999999999999)}`);
+    else {
+        document.body.appendChild(document.createElement('script')).src='https://127.0.0.1:8010/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
     }
-})
+})();
