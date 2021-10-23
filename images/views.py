433948120d925d22eda18891258537aee06f7f27
Alm4my <thenixadm@gmail.com>
2021-10-23 15:54:00 +0000

    Add urls and view for creating an image

--- images/views.py
+++ images/views.py
@@ -1,3 +1,22 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from images.forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+    else:
+        form = ImageCreateForm(data=request.GET)
+        
+    return render(request, 'images/image/create.html',
+                  {'section': 'images', 'form': form})
