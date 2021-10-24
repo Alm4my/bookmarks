94baa29267d936d74435822cfe0c36bf535acf97
Alm4my <thenixadm@gmail.com>
2021-10-24 00:31:55 +0000

    Create ajax request to update numbers of like on an image

--- images/urls.py
+++ images/urls.py
@@ -6,5 +6,6 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', views.image_create, name='create'),
+    path('like/', views.image_like, name='like'),
     path('detail/<int:image_id>/<slug:slug>', views.image_detail, name='detail'),
 ]
