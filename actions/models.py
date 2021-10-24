d1d07c77c6644c0fb4c43373035edb09d33626b6
Alm4my <thenixadm@gmail.com>
2021-10-24 02:28:34 +0000

    Add ContentType foreign key to action, display it in admin, and migrate

--- actions/models.py
+++ actions/models.py
@@ -1,10 +1,15 @@
-from django.db.models import Model, ForeignKey, CASCADE, CharField, DateTimeField
-from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Model, ForeignKey, CASCADE, CharField, DateTimeField, PositiveIntegerField
 
 
 class Action(Model):
     user = ForeignKey('auth.User', related_name='actions', db_index=True, on_delete=CASCADE)
     verb = CharField(max_length=255)
+    target_ct = ForeignKey(ContentType, blank=True, null=True, related_name='target_obj',
+                           on_delete=CASCADE)
+    target_id = PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_ct', 'target_id')
     created = DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
