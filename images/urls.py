5224b62a3be97fc1d8f97eda93e4dbc2dcd33aa0
Alm4my <thenixadm@gmail.com>
2021-10-24 04:36:57 +0000

    Store ranking with redis and display the ranking of the most viewed images

--- images/urls.py
+++ images/urls.py
@@ -6,6 +6,7 @@ app_name = 'images'
 
 urlpatterns = [
     path('', views.image_list, name='list'),
+    path('ranking/', views.image_ranking, name='ranking'),
     path('create/', views.image_create, name='create'),
     path('like/', views.image_like, name='like'),
     path('detail/<int:image_id>/<slug:slug>', views.image_detail, name='detail'),
--- images/views.py
+++ images/views.py
@@ -49,11 +49,25 @@ def image_detail(request, image_id, slug):
     image = get_object_or_404(Image, id=image_id, slug=slug)
     # increment total image views by 1
     total_views = r.incr(f'image:{image.id}:views')
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image,
                    'total_views': total_views})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking_ = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id_) for id_ in image_ranking_]
+    # get the most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'images',
+                                                         'most_viewed': most_viewed})
 
 
 @ajax_required
